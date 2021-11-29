d56e057d4e7f3fd036a4dc59d1808288ba5991a6
AbdulQadir97 <aq201541@gmail.com>
2021-11-29 06:31:39 +0500

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,37 @@
 import { Link } from "react-router-dom";
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 const Home = ()=>{
 return(
-    <div>
-    <h1>Home Page</h1>
-    <nav>
-    <Link to="/">Signup</Link>
-  </nav>
-  </div>
+
+  <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['3']}>
+        <Menu.Item key="1"><Link to="/">Signup</Link></Menu.Item>
+        <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
+        <Menu.Item key="3"><Link to="/home">Home</Link></Menu.Item>
+      </Menu>
+    </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Signup</Breadcrumb.Item>
+        <Breadcrumb.Item>Login</Breadcrumb.Item>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        <h2>TEAM REPORTER APP</h2>
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Created By Friends &amp; Tech</Footer>
+  </Layout>
+  //   <div>
+  //   <h1>Home Page</h1>
+  //   <nav>
+  //   <Link to="/">Signup</Link>
+  // </nav>
+  // </div>
 )
 }
 export default Home;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,13 +1,90 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import '../styles/formStyle.css'
+import { Layout } from 'antd';
+import { Form, Input, Button } from 'antd';
+const { Header } = Layout;
 
-const Signup = ()=>{
-return(
+const Signup = () => {
+    const navigate = useNavigate()
+    const signupHandler = () => {  
+      navigate('../login')
+      }
+
+      let userEmailRef = useRef('Null')
+      let userEmail = userEmailRef.current.value;
+      console.log(userEmail)
+  return (
+
     <div>
-        <h1>Signup Page</h1>
-    <nav>
-    <Link to="/login">Signup</Link>
-  </nav>
-  </div>
-)
+      <Header>
+        <h1 >SIGNUP FORM</h1>
+      </Header>
+
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+
+      >
+
+        <Form.Item
+
+          label="Useremail"
+          name="useremail"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button onClick = {signupHandler} type="primary" htmlType="submit">
+            Signup Now
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+
+  )
 }
 export default Signup;
